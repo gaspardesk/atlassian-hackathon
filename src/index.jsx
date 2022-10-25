840e577ca640fe9902d967499e6b023ca369febe
Aris Miletis <aris@gaspardesk.com>
2022-10-25 15:47:26 +0300

    encrypted headers

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import api, { route, fetch } from "@forge/api";
 import { view } from '@forge/bridge';
 import ForgeUI, { render, QueuePage, Fragment, Text, useProductContext, useState, TextField, Form } from "@forge/ui";
+import * as crypto from 'crypto';
 import axios from "axios";
 
 
@@ -8,47 +9,49 @@ const App = () => {
     // useState is a UI kit hook we use to manage the form data in local state
     const [formState, setFormState] = useState(undefined);
 
+    function signKey (message) {
+      return crypto.createHmac('sha256', Buffer.from(process.env.HMAC_KEY, 'hex')).update(message).digest('hex');
+  }
+
     // Handles form submission, which is a good place to call APIs, or to set component state...
     const onSubmit = async (formData) => {
-      // const res = await axios.get(`https://8d16-94-66-221-143.eu.ngrok.io/common/jira_metrics`);
-      const response = await api.asUser().requestJira(
-        route`/rest/api/3/myself`
-      );
-      const user = await response.json();
-      const domain = user.emailAddress.split("@")[1];
-      const domain_name = domain.split(".")[0]
-      setFormState(domain_name);
-    };
-
-    const checkIfSignedUp = async () => {
       const response = await api.asUser().requestJira(
         route`/rest/api/3/myself`
       );
       const user = await response.json();
       const organization_domain = user.emailAddress.split("@")[1];
-      const domain_name = organization_domain.split(".")[0]
-      axios.get(`https://${domain_name}.gaspardesk.com/api/common/info/version`)
-      .then((response) =>{
-        return true;
-      })
-      .catch((reason) => {
-        if (reason.response.status === 404){
-          return false;
-        }
-      })
+      const domain_name = organization_domain.split(".")[0];
+      const now = new Date().toISOString();
+      const signature = signKey(domain_name + now);
+      let logged_in = "Unknown";
+      try{
+        const response = await axios.get(`https://6fd0-94-66-221-143.eu.ngrok.io/common/info/version`, {
+          headers: {
+            "X-Forge-payload": `${domain_name}${now}`,
+            "X-Forge-signature": signature,
+          }
+        });
+        console.log(response.statusText);
+        logged_in = "You have signed Up";
+      }
+      catch (error) {
+        console.log(error.message);
+        logged_in = "You have NOT signed Up";
+      }
 
-    }
+      
+      setFormState(logged_in);
+    };
+
 
 
   return (
     <Fragment>
-      <Text>Hello world!</Text>
       <Fragment>
         {formState && <Text>{JSON.stringify(formState)}</Text>}</Fragment>
       <Form onSubmit={onSubmit}>
       <TextField label="Message" name="message" placeholder="Type your message here"/>
       </Form>
-      {checkIfSignedUp && <Text>You are signed up!</Text>}
     </Fragment>
   );
 };
